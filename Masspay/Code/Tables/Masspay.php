6b3213f3508b308dba19deff6d098ee3cc6444e9
dedanirungu <dedanirungu@gmail.com>
2017-08-04 13:23:06 +0300

    Adding Masspay generation files

---
+++ Masspay/Code/Tables/Masspay.php
@@ -0,0 +1,322 @@
+<?php
+
+namespace Finance\Masspay\Code\Tables;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Masspay
+ *
+ * @ORM\Table(name="finance_masspay", indexes={@ORM\Index(name="created_by_index", columns={"created_by"}), @ORM\Index(name="modified_by_index", columns={"modified_by"})})
+ * @ORM\Entity
+ * @ORM\HasLifecycleCallbacks
+ */
+class Masspay extends \Kazist\Table\BaseTable {
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    protected $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="year", type="string", length=255, nullable=false)
+     */
+    protected $year;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="month", type="string", length=255, nullable=false)
+     */
+    protected $month;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="date", type="string", length=255, nullable=false)
+     */
+    protected $date;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="token", type="string", length=255, nullable=false)
+     */
+    protected $token;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="is_processed", type="integer", length=11, nullable=true)
+     */
+    protected $is_processed;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="type", type="string", length=255, nullable=true)
+     */
+    protected $type;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="max_limit", type="integer", length=11, nullable=true)
+     */
+    protected $max_limit;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="file", type="string", length=255, nullable=true)
+     */
+    protected $file;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="created_by", type="integer", length=11, nullable=false)
+     */
+    protected $created_by;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date_created", type="datetime", nullable=false)
+     */
+    protected $date_created;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="modified_by", type="integer", length=11, nullable=false)
+     */
+    protected $modified_by;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date_modified", type="datetime", nullable=false)
+     */
+    protected $date_modified;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId() {
+        return $this->id;
+    }
+
+    /**
+     * Set year
+     *
+     * @param string $year
+     * @return Masspay
+     */
+    public function setYear($year) {
+        $this->year = $year;
+
+        return $this;
+    }
+
+    /**
+     * Get year
+     *
+     * @return string 
+     */
+    public function getYear() {
+        return $this->year;
+    }
+
+    /**
+     * Set month
+     *
+     * @param string $month
+     * @return Masspay
+     */
+    public function setMonth($month) {
+        $this->month = $month;
+
+        return $this;
+    }
+
+    /**
+     * Get month
+     *
+     * @return string 
+     */
+    public function getMonth() {
+        return $this->month;
+    }
+
+    /**
+     * Set date
+     *
+     * @param string $date
+     * @return Masspay
+     */
+    public function setDate($date) {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return string 
+     */
+    public function getDate() {
+        return $this->date;
+    }
+
+    /**
+     * Set token
+     *
+     * @param string $token
+     * @return Masspay
+     */
+    public function setToken($token) {
+        $this->token = $token;
+
+        return $this;
+    }
+
+    /**
+     * Get token
+     *
+     * @return string 
+     */
+    public function getToken() {
+        return $this->token;
+    }
+
+    /**
+     * Set is_processed
+     *
+     * @param integer $isProcessed
+     * @return Masspay
+     */
+    public function setIsProcessed($isProcessed) {
+        $this->is_processed = $isProcessed;
+
+        return $this;
+    }
+
+    /**
+     * Get is_processed
+     *
+     * @return integer 
+     */
+    public function getIsProcessed() {
+        return $this->is_processed;
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     * @return Masspay
+     */
+    public function setType($type) {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string 
+     */
+    public function getType() {
+        return $this->type;
+    }
+
+    /**
+     * Set max_limit
+     *
+     * @param integer $maxLimit
+     * @return Masspay
+     */
+    public function setMaxLimit($maxLimit) {
+        $this->max_limit = $maxLimit;
+
+        return $this;
+    }
+
+    /**
+     * Get max_limit
+     *
+     * @return integer 
+     */
+    public function getMaxLimit() {
+        return $this->max_limit;
+    }
+
+    /**
+     * Set file
+     *
+     * @param string $file
+     * @return Masspay
+     */
+    public function setFile($file) {
+        $this->file = $file;
+
+        return $this;
+    }
+
+    /**
+     * Get file
+     *
+     * @return string 
+     */
+    public function getFile() {
+        return $this->file;
+    }
+
+    /**
+     * Get created_by
+     *
+     * @return integer 
+     */
+    public function getCreatedBy() {
+        return $this->created_by;
+    }
+
+    /**
+     * Get date_created
+     *
+     * @return \DateTime 
+     */
+    public function getDateCreated() {
+        return $this->date_created;
+    }
+
+    /**
+     * Get modified_by
+     *
+     * @return integer 
+     */
+    public function getModifiedBy() {
+        return $this->modified_by;
+    }
+
+    /**
+     * Get date_modified
+     *
+     * @return \DateTime 
+     */
+    public function getDateModified() {
+        return $this->date_modified;
+    }
+
+}
